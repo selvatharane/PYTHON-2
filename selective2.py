6c58429807e492cd1060c043785c4d95d45eeb5f
selvatharane <130639350+selvatharane@users.noreply.github.com>
2023-05-16 22:52:39 +0530

    Delete selective2.py

--- selective2.py
+++
@@ -1,7 +0,0 @@
-a=int(input("enter first side of a triangle:"))
-b=int(input("enter second side of a triangle:"))
-c=int(input("enter third side of a triangle:"))
-if(((a+c)>b) and((a+b)>c) and ((b+c)>a)):
-    print("the triange is possible")
-else:
-    print("the triange is not possible")
