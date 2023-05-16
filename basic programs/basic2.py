31b0b27d892642c4dba33660d090a8aaca0d4104
selvatharane <130639350+selvatharane@users.noreply.github.com>
2023-05-16 22:38:03 +0530

    Add files via upload

---
+++ basic programs/basic2.py
@@ -0,0 +1,7 @@
+rad=int(input("enter radius:"))
+height=int(input("enter height:"))
+pi=3.14
+volume=pi*rad*rad*height
+surface_area=((2*pi*rad)*height)+((pi*rad**2)*2)
+print("the volume of the cylinder is:",volume)
+print("the surface area of the cylinder is:",surface_area)
