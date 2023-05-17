4323ab6b8367702253d54edb204c9442675bf4ee
selvatharane <130639350+selvatharane@users.noreply.github.com>
2023-05-17 19:04:21 +0530

    Add files via upload

---
+++ looping programs/looping6.py
@@ -0,0 +1,14 @@
+for i in range(5,9):
+    if(i==5):
+        for j in range(1,6):
+            print(i,"*",j,"=",i*j)
+    if(i==6):
+        for j in range(1,7):
+            print(i,"*",j,"=",i*j)
+    if(i==7):
+        for j in range(1,8):
+            print(i,"*",j,"=",i*j)
+    if(i==8):
+        for j in range(1,9):
+            print(i,"*",j,"=",i*j)
+    
