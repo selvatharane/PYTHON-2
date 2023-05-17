13c7d44015cb50562994b6bc83f23a8610132bef
selvatharane <130639350+selvatharane@users.noreply.github.com>
2023-05-17 20:55:06 +0530

    Add files via upload

---
+++ looping programs/looping9.py
@@ -0,0 +1,11 @@
+even_count=0
+odd_count=0
+num=0
+while(num!=-1):
+    num=int(input("enter:"))
+    if(num%2==0):
+        even_count+=1
+    else:
+        odd_count+=1
+print(even_count)
+print(odd_count-1)
