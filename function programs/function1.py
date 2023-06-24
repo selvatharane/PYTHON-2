966439fe5ddf130c728cd406a0993850f7e189ad
selvatharane <130639350+selvatharane@users.noreply.github.com>
2023-06-24 10:11:38 +0530

    Add files via upload

---
+++ function programs/function1.py
@@ -0,0 +1,14 @@
+'''Write a python code which checks whether the input number is a Perfect Square of not using a function.'''
+
+def square(n):
+  i=1
+  while(i<n):
+     if(i**2==n):
+       return o
+     i+=1
+n=int(input("enter:"))
+c=square(n)
+if(c==0):
+   print("perfect square")
+else:
+   print("not a perfect square")
